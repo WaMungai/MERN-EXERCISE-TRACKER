c03824e050480797215c6721dd791949920902e8
unknown <joyce.mungai@simpleformations.ke>
2022-09-17 00:23:41 +0300

    Add logic for the user and exercise files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
 
-require("dotenv").config();
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +11,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {  }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
 app.listen(port, () => {
-  console.log(`Server listening on: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
+
