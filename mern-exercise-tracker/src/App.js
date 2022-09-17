6024ff9610b3907e07d7d2cc28a52bb85904ac80
unknown <joyce.mungai@simpleformations.ke>
2022-09-17 12:31:33 +0300

    Create class based components

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import ExerciseList from './components/ExerciseList';
+import EditExercise from './components/EditExercise';
+import CreateExercise from './components/CreateExercise';
+import CreateUser from './components/CreateUser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Navbar />
+     <br />
+     <Routes>
+     <Route path="/" exact element={<ExerciseList />} />
+     <Route path="/edit/:id"  element={<EditExercise />} />
+     <Route path="/create"  element={<CreateExercise />} />
+     <Route path="/user"  element={<CreateUser />} />
+     </Routes>
+    </BrowserRouter>
+    
   );
 }
 
